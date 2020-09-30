f79922877060ee158b44f729e0e17128da8863e5
David Pine <david.pine@microsoft.com>
2020-09-30 14:22:28 -0500

    Remove try/catch and let errors bubble.

--- Microsoft.Azure.CosmosRepository/src/DefaultRepository.cs
+++ Microsoft.Azure.CosmosRepository/src/DefaultRepository.cs
@@ -1,15 +1,14 @@
 ﻿// Copyright (c) IEvangelist. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.Azure.Cosmos;
-using Microsoft.Azure.Cosmos.Linq;
-using Microsoft.Azure.CosmosRepository.Providers;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Linq;
+using Microsoft.Azure.CosmosRepository.Providers;
 
 namespace Microsoft.Azure.CosmosRepository
 {
@@ -17,57 +16,38 @@ namespace Microsoft.Azure.CosmosRepository
     internal class DefaultRepository<TItem> : IRepository<TItem> where TItem : Item
     {
         readonly ICosmosContainerProvider _containerProvider;
-        readonly ILogger<DefaultRepository<TItem>> _logger;
 
-        public DefaultRepository(
-            ICosmosContainerProvider containerProvider,
-            ILogger<DefaultRepository<TItem>> logger) =>
-            (_containerProvider, _logger) = (containerProvider, logger);
+        public DefaultRepository(ICosmosContainerProvider containerProvider) =>
+            _containerProvider = containerProvider;
 
         /// <inheritdoc/>
         public async ValueTask<TItem> GetAsync(string id)
         {
-            try
-            {
-                Container container = await _containerProvider.GetContainerAsync();
-                ItemResponse<TItem> response = await container.ReadItemAsync<TItem>(id, new PartitionKey(id));
+            Container container = await _containerProvider.GetContainerAsync();
+            ItemResponse<TItem> response = await container.ReadItemAsync<TItem>(id, new PartitionKey(id));
 
-                return response.Resource;
-            }
-            catch (CosmosException ex)
-            {
-                _logger.LogError(ex.Message, ex);
-                return default;
-            }
+            return response.Resource;
         }
 
         /// <inheritdoc/>
         public async ValueTask<IEnumerable<TItem>> GetAsync(Expression<Func<TItem, bool>> predicate)
         {
-            try
+            Container container = await _containerProvider.GetContainerAsync();
+            using (FeedIterator<TItem> iterator =
+                container.GetItemLinqQueryable<TItem>()
+                         .Where(predicate)
+                         .ToFeedIterator())
             {
-                Container container = await _containerProvider.GetContainerAsync();
-                using (FeedIterator<TItem> iterator =
-                    container.GetItemLinqQueryable<TItem>()
-                             .Where(predicate)
-                             .ToFeedIterator())
+                List<TItem> results = new List<TItem>();
+                while (iterator.HasMoreResults)
                 {
-                    List<TItem> results = new List<TItem>();
-                    while (iterator.HasMoreResults)
+                    foreach (TItem result in await iterator.ReadNextAsync())
                     {
-                        foreach (TItem result in await iterator.ReadNextAsync())
-                        {
-                            results.Add(result);
-                        }
+                        results.Add(result);
                     }
-
-                    return results;
                 }
-            }
-            catch (CosmosException ex)
-            {
-                _logger.LogError(ex.Message, ex);
-                return Enumerable.Empty<TItem>();
+
+                return results;
             }
         }
 
