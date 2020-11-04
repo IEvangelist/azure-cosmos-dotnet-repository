f36c14f6dc58578c1477c2c5c533439b5aad6783
Invvard <7305493+Invvard@users.noreply.github.com>
2020-11-03 21:53:36 -0500

    Add DefaultCosmosClientOptionsProvider implementation

---
+++ Microsoft.Azure.CosmosRepository/src/Providers/DefaultCosmosClientOptionsProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.CosmosRepository.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Azure.CosmosRepository.Providers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DefaultCosmosClientOptionsProvider : ICosmosClientOptionsProvider
+    {
+        private readonly Lazy<CosmosClientOptions> _lazyClientOptions;
+
+        /// <inheritdoc />
+        public CosmosClientOptions GetClientOptions => _lazyClientOptions.Value;
+
+        /// <summary>
+        /// Default <see cref="ICosmosClientOptionsProvider"/> implementation.
+        /// </summary>
+        /// <param name="serviceProvider">Service provider implementation.</param>
+        /// <param name="configuration">Service configuration implementation.</param>
+        public DefaultCosmosClientOptionsProvider(IServiceProvider serviceProvider, IConfiguration configuration)
+        {
+            _lazyClientOptions = new Lazy<CosmosClientOptions>(() => CreateCosmosClientOptions(serviceProvider, configuration));
+        }
+
+        private CosmosClientOptions CreateCosmosClientOptions(IServiceProvider serviceProvider, IConfiguration configuration)
+        {
+            IHttpClientFactory factory = serviceProvider.GetRequiredService<IHttpClientFactory>();
+
+            return new CosmosClientOptions
+                   {
+                       HttpClientFactory = () =>
+                                           {
+                                               HttpClient client = factory.CreateClient();
+
+                                               string version =
+                                                   Assembly.GetExecutingAssembly()
+                                                           .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                                                           .InformationalVersion;
+
+                                               client.DefaultRequestHeaders
+                                                     .UserAgent
+                                                     .ParseAdd($"ievangelist-cosmos-repository-sdk/{version}");
+
+                                               return client;
+                                           },
+                       AllowBulkExecution = configuration.GetSection(nameof(RepositoryOptions))
+                                                         .GetValue<bool>(nameof(RepositoryOptions.AllowBulkExecution))
+                   };
+        }
+    }
+}
