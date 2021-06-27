2bd7919ac4b685357b131e64af1e74940f344348
Michael Zhang <zhangzunke@126.com>
2021-06-27 09:49:32 +0800

    Update Microsoft.Azure.CosmosRepository/src/Dtos/ListResult.cs
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- Microsoft.Azure.CosmosRepository/src/Dtos/ListResult.cs
+++ Microsoft.Azure.CosmosRepository/src/Dtos/ListResult.cs
@@ -16,10 +16,7 @@ namespace Microsoft.Azure.CosmosRepository.Pagination
         /// Creates a new <see cref="ListResult{T}"/> object.
         /// </summary>
         /// <param name="items">List of items</param>
-        public ListResult(IReadOnlyList<T> items)
-        {
-            Items = items;
-        }
+        public ListResult(IReadOnlyList<T> items) =>Items = items;
 
         /// <inheritdoc />
         public IReadOnlyList<T> Items { get; internal set; } = new List<T>();
