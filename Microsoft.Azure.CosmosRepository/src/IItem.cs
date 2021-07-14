f80fb55725e29a8c6cabe6f4c9ee7d547718b9ac
David Cuccia <dcuccia@gmail.com>
2021-07-14 15:50:13 -0700

    Remove unused using statement

--- Microsoft.Azure.CosmosRepository/src/IItem.cs
+++ Microsoft.Azure.CosmosRepository/src/IItem.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) IEvangelist. All rights reserved.
 // Licensed under the MIT License.
 
-using Microsoft.Azure.Cosmos;
-
 namespace Microsoft.Azure.CosmosRepository
 {
     /// <summary>
