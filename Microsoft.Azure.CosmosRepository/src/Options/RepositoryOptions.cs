bddb00b2dec6e28c6e747675fcb0368b35e41581
David Pine <david.pine@microsoft.com>
2021-07-19 10:31:04 -0500

    Move options object out

--- Microsoft.Azure.CosmosRepository/src/Options/RepositoryOptions.cs
+++ Microsoft.Azure.CosmosRepository/src/Options/RepositoryOptions.cs
@@ -70,30 +70,4 @@ namespace Microsoft.Azure.CosmosRepository.Options
         /// </summary>
         public RepositorySerializationOptions SerializationOptions { get; set; }
     }
-
-    /// <summary>
-    /// The serialization options for the Cosmos DB repository.
-    /// These are mapped to the <see cref="CosmosClientOptions.SerializerOptions"/> object.
-    /// </summary>
-    public class RepositorySerializationOptions
-    {
-        /// <summary>
-        /// Gets or sets if the serializer should ignore null properties.
-        /// </summary>
-        /// <remarks>The default value is false</remarks>
-        public bool IgnoreNullValues { get; set; }
-
-        /// <summary>
-        /// Gets or sets if the serializer should use indentation.
-        /// </summary>
-        /// <remarks>The default value is false</remarks>
-        public bool Indented { get; set; }
-
-        /// <summary>
-        /// Gets or sets whether the naming policy used to convert a string-based name to
-        /// another format, such as a camel-casing format.
-        /// </summary>
-        /// <remarks>The default value is <see cref="CosmosPropertyNamingPolicy.CamelCase"/>.</remarks>
-        public CosmosPropertyNamingPolicy PropertyNamingPolicy { get; set; } = CosmosPropertyNamingPolicy.CamelCase;
-    }
 }
