0d76211ad1fec6ebd2a0a17f0e6424f3f985858b
Matt Stannett <matt.stannett@awaregroup.com>
2021-10-19 16:23:35 +1300

    Adds missing client provider tests

--- Microsoft.Azure.CosmosRepository/test/Providers/DefaultCosmosClientProviderTests.cs
+++ Microsoft.Azure.CosmosRepository/test/Providers/DefaultCosmosClientProviderTests.cs
@@ -5,6 +5,7 @@ using System;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.CosmosRepository.Options;
 using Microsoft.Azure.CosmosRepository.Providers;
+using Microsoft.Azure.CosmosRepositoryTests.Stubs;
 using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
@@ -14,13 +15,26 @@ namespace Microsoft.Azure.CosmosRepositoryTests.Providers
     public class DefaultCosmosClientProviderTests
     {
         [Fact]
-        public void NewDefaultCosmosClientProviderThrowsWithNullCosmosClientOptionsProvider() =>
+        public void NewDefaultCosmosClientProviderThrowsWithNullCosmosClientOptionsProviderAndValidConnectionStringOptions() =>
            Assert.Throws<ArgumentNullException>(
                () => new DefaultCosmosClientProvider(
                    cosmosClientOptionsProvider: null,
                    Microsoft.Extensions.Options.Options.Create(new RepositoryOptions
                    {
                        CosmosConnectionString = "pickles",
+                       DatabaseId = "data",
+                       ContainerId = "container"
+                   })));
+
+        [Fact]
+        public void NewDefaultCosmosClientProviderThrowsWithNullCosmosClientOptionsProviderAndValidTokenCredentialOptions() =>
+           Assert.Throws<ArgumentNullException>(
+               () => new DefaultCosmosClientProvider(
+                   cosmosClientOptionsProvider: null,
+                   Microsoft.Extensions.Options.Options.Create(new RepositoryOptions
+                   {
+                       TokenCredential = new TestTokenCredential(),
+                       AccountEndpoint = "pickles endpoint",
                        DatabaseId = "data",
                        ContainerId = "container"
                    })));
@@ -33,7 +47,7 @@ namespace Microsoft.Azure.CosmosRepositoryTests.Providers
                    null));
 
         [Fact]
-        public void DefaultCosmosClientProviderCorrectlyDisposesOverload()
+        public void DefaultCosmosClientProviderCorrectlyDisposesOverloadWithConnectionString()
         {
             Mock<ICosmosClientOptionsProvider> mock = new Mock<ICosmosClientOptionsProvider>();
             mock.SetupGet(provider => provider.ClientOptions).Returns(new CosmosClientOptions());
@@ -44,6 +58,26 @@ namespace Microsoft.Azure.CosmosRepositoryTests.Providers
                     {
                         CosmosConnectionString =
                             "AccountEndpoint=https://localtestcosmos.documents.azure.com:443/;AccountKey=RmFrZUtleQ==;"
+                    }));
+
+            provider.Dispose();
+
+            Assert.ThrowsAsync<ObjectDisposedException>(
+                async () => await provider.UseClientAsync(client => client.ReadAccountAsync()));
+        }
+
+        [Fact]
+        public void DefaultCosmosClientProviderCorrectlyDisposesOverloadWithTokenCredential()
+        {
+            Mock<ICosmosClientOptionsProvider> mock = new Mock<ICosmosClientOptionsProvider>();
+            mock.SetupGet(provider => provider.ClientOptions).Returns(new CosmosClientOptions());
+            DefaultCosmosClientProvider provider =
+                new DefaultCosmosClientProvider(
+                    mock.Object,
+                    Microsoft.Extensions.Options.Options.Create(new RepositoryOptions
+                    {
+                        TokenCredential = new TestTokenCredential(),
+                        AccountEndpoint = "https://localtestcosmos.documents.azure.com:443/"
                     }));
 
             provider.Dispose();
