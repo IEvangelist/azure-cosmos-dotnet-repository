d6ee99f170157cc309c695b0855405054d3f5b37
Billy Mumby <billy.mumby@outlook.com>
2022-01-08 20:42:43 +0000

    some XML comments

--- Microsoft.Azure.CosmosRepository/src/ChangeFeed/InMemory/InMemoryChangeFeed.cs
+++ Microsoft.Azure.CosmosRepository/src/ChangeFeed/InMemory/InMemoryChangeFeed.cs
@@ -6,26 +6,23 @@ using System;
 namespace Microsoft.Azure.CosmosRepository.ChangeFeed.InMemory
 {
     /// <summary>
-    ///
+    /// Represents a change feed for a given <see cref="IItem"/>
     /// </summary>
     public class InMemoryChangeFeed<TItem> where TItem : class, IItem
     {
         private readonly IRepository<TItem> _repository;
         private readonly IItemChangeFeedProcessor<TItem> _changeFeedProcessor;
 
-        /// <summary>
-        ///
-        /// </summary>
-        public InMemoryChangeFeed(IRepository<TItem> repository, IItemChangeFeedProcessor<TItem> changeFeedProcessor)
+        internal InMemoryChangeFeed(IRepository<TItem> repository, IItemChangeFeedProcessor<TItem> changeFeedProcessor)
         {
             _repository = repository;
             _changeFeedProcessor = changeFeedProcessor;
         }
 
         /// <summary>
-        ///
+        /// Set's up the <see cref="InMemoryRepository{TItem}"/> to start listen to changes from the given <see cref="InMemoryRepository{TItem}"/>
         /// </summary>
-        /// <exception cref="InvalidOperationException"></exception>
+        /// <exception cref="InvalidOperationException">Thrown when the <see cref="IRepository{TItem}"/> registered in DI is not a <see cref="InMemoryRepository{TItem}"/></exception>
         public void Setup()
         {
             if (_repository is InMemoryRepository<TItem> inMemoryRepository)
