27ebda64aa3a53476b3f9cb92e9a4dc6c1f1655a
Billy Mumby <billy.mumby@outlook.com>
2022-01-10 15:26:51 +0000

    Update Microsoft.Azure.CosmosRepository/src/ChangeFeed/DefaultConatinerChangeFeedProcessor.cs
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- Microsoft.Azure.CosmosRepository/src/ChangeFeed/DefaultConatinerChangeFeedProcessor.cs
+++ Microsoft.Azure.CosmosRepository/src/ChangeFeed/DefaultConatinerChangeFeedProcessor.cs
@@ -121,25 +121,26 @@ namespace Microsoft.Azure.CosmosRepository.ChangeFeed
             _logger.LogDebug("Invoking IItemChangeFeedProcessor's ({ProcessorsCount}) for item type {ItemType}",
                 handlers.Count(), itemType);
 
-            foreach (object handler in handlers)
-            {
-                try
+            await Task.WhenAll(
+                handlers.Select(hanlder => 
                 {
-                    object response = handlerType.GetMethod("HandleAsync")?
-                        .Invoke(handler, new[] { item, cancellationToken });
-
-                    if (response is ValueTask valueTask)
+                    try
                     {
-                        await valueTask;
+                        object response = handlerType.GetMethod("HandleAsync")?
+                            .Invoke(handler, new[] { item, cancellationToken });
+    
+                        if (response is ValueTask valueTask)
+                        {
+                            await valueTask;
+                        }
                     }
-                }
-                catch (Exception e)
-                {
-                    _logger.LogError(e,
-                        "Failed to handle change for item of type {ItemType} when invoking IItemChangeFeedProcessor {ProcessorTypeName}",
-                        itemType, handler.GetType().Name);
-                }
-            }
+                    catch (Exception e)
+                    {
+                        _logger.LogError(e,
+                            "Failed to handle change for item of type {ItemType} when invoking IItemChangeFeedProcessor {ProcessorTypeName}",
+                            itemType, handler.GetType().Name);
+                    }
+                }));
         }
 
         private Task OnErrorAsync(Exception exception, string containerName)
