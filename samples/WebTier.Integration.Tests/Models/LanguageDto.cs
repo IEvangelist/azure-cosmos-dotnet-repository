be5c5c886b978b6f86d9ebf67c1b7b913ec16b72
Billy Mumby <billy.mumby@outlook.com>
2022-02-10 19:22:57 +0000

    Update samples/WebTier.Integration.Tests/Models/LanguageDto.cs
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- samples/WebTier.Integration.Tests/Models/LanguageDto.cs
+++ samples/WebTier.Integration.Tests/Models/LanguageDto.cs
@@ -8,7 +8,7 @@ namespace WebTier.Integration.Tests
 {
     public class LanguageDto
     {
-        public string Id { get; set; } = "default";
+        public string Id { get; set; } = null!;
 
         public string Name { get; set; }
 
