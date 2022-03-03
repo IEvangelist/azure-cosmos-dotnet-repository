29ac5d7009d11fee0d6e406155c17f8486d03d77
Billy Mumby <billy.mumby@outlook.com>
2022-03-03 21:11:49 +0000

    Refactor to partial classes.

--- samples/BasicEventSourcingSample/Core/ShipApplys.cs
+++ samples/BasicEventSourcingSample/Core/ShipApplys.cs
@@ -5,15 +5,8 @@ using Microsoft.Azure.CosmosEventSourcing;
 
 namespace BasicEventSourcingSample.Core;
 
-public class Ship : Aggregate
+public partial class Ship
 {
-    public string Name { get; private set; } = null!;
-    public DateTime Commissioned { get; private set; }
-    public DateTime CreatedAt { get; private set; }
-    public ShipStatus Status { get; private set; }
-    public string? Port { get; private set; }
-    public double? CargoWeight { get; private set; }
-
     private Ship() { }
 
     public static Ship Build(List<IPersistedEvent> persistedEvents)
@@ -22,21 +15,6 @@ public class Ship : Aggregate
         ship.Apply(persistedEvents);
         return ship;
     }
-
-    public Ship(string name, DateTime commissioned, DateTime? createdAt = null) =>
-        AddEvent(new ShipEvents.ShipCreated(name, commissioned, createdAt ?? DateTime.UtcNow));
-
-    public void Dock(string port, DateTime occuredUtc) =>
-        AddEvent(new ShipEvents.DockedInPort(Name, port, occuredUtc));
-
-    public void StartLoading(string port, DateTime occuredUtc) =>
-        AddEvent(new ShipEvents.Loading(Name, port, occuredUtc));
-
-    public void FinishLoading(string port, double cargoWeight, DateTime occuredUtc) =>
-        AddEvent(new ShipEvents.Loaded(Name, port, cargoWeight, occuredUtc));
-
-    public void Depart(string port, DateTime occuredUtc) =>
-        AddEvent(new ShipEvents.Departed(Name, port, occuredUtc));
 
     private void Apply(ShipEvents.ShipCreated shipCreated)
     {
