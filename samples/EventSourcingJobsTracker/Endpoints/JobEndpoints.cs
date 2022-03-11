5b37e2eafbd8c0365b592ae97f0b89490c64d41f
Billy Mumby <billy.mumby@outlook.com>
2022-03-11 08:34:04 +0000

    Refactored into Endpoints extensions.

---
+++ samples/EventSourcingJobsTracker/Endpoints/JobEndpoints.cs
@@ -0,0 +1,71 @@
+// Copyright (c) IEvangelist. All rights reserved.
+// Licensed under the MIT License.
+
+using CleanArchitecture.Exceptions.AspNetCore;
+using EventSourcingJobsTracker.API.DTOs;
+using EventSourcingJobsTracker.API.Requests;
+using EventSourcingJobsTracker.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventSourcingJobsTracker.Endpoints;
+
+public static class JobEndpoints
+{
+    public const string Tag = "Jobs";
+    public static IEndpointRouteBuilder MapJobEndpoints(this IEndpointRouteBuilder builder)
+    {
+        builder.MapPost(
+                "/api/jobs-list/jobs/",
+                async (
+                    CreateJob request,
+                    IJobListService service) =>
+                {
+                    (Guid jobListId, string? title, DateTime due) = request;
+
+                    await service.AddJob(jobListId, title, due);
+
+                    return Results.Ok();
+                })
+            .Accepts<CreateJob>("application/json")
+            .Produces(200)
+            .Produces<ErrorResponse>(400)
+            .Produces<ErrorResponse>(404)
+            .WithTags(Tag);
+
+        builder.MapPut(
+                "/api/jobs-list/jobs/complete",
+                async (
+                    CompleteJob request,
+                    IJobListService service) =>
+                {
+                    (Guid jobListId, Guid jobId) = request;
+
+                    await service.CompleteJob(jobListId, jobId);
+
+                    return Results.Ok();
+                })
+            .Accepts<CompleteJob>("application/json")
+            .Produces(200)
+            .Produces<ErrorResponse>(400)
+            .Produces<ErrorResponse>(404)
+            .WithTags(Tag);
+
+        builder.MapGet(
+                "/api/jobs-list/jobs/",
+                async (
+                    Guid jobListId,
+                    [FromServices] IJobTrackerReadService readService) =>
+                {
+                    IEnumerable<JobDto> jobs = await readService.FindJobsForJobsListAsync(jobListId);
+
+                    return jobs.Any()
+                        ? Results.Ok(jobs)
+                        : Results.NoContent();
+                })
+            .Produces(200)
+            .Produces(204)
+            .WithTags(Tag);
+
+        return builder;
+    }
+}
--- samples/EventSourcingJobsTracker/Program.cs
+++ samples/EventSourcingJobsTracker/Program.cs
@@ -1,12 +1,10 @@
 using CleanArchitecture.Exceptions.AspNetCore;
-using EventSourcingJobsTracker.API.DTOs;
-using EventSourcingJobsTracker.API.Requests;
 using EventSourcingJobsTracker.Application.Infrastructure;
 using EventSourcingJobsTracker.Application.Services;
 using EventSourcingJobsTracker.Core.Aggregates;
+using EventSourcingJobsTracker.Endpoints;
 using EventSourcingJobsTracker.Infrastructure.Items;
 using EventSourcingJobsTracker.Infrastructure.Repositories;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.CosmosEventSourcing.Extensions;
 using Microsoft.Azure.CosmosRepository.AspNetCore.Extensions;
 
@@ -60,109 +58,7 @@ app
     .MapGet("/", () => Results.Redirect("/swagger"))
     .ExcludeFromDescription();
 
-app.MapPost(
-        "/api/jobs-list/",
-        async (
-            CreateJobList request,
-            IJobListService service) =>
-        {
-            (string name, string category, string username) = request;
-
-            Guid id = await service.CreateJobList(
-                name,
-                category,
-                username);
-
-            return Results.Created($"api/jobs-list/{id}", id);
-        })
-    .Accepts<CreateJobList>("application/json")
-    .Produces(201)
-    .Produces<ErrorResponse>(400)
-    .WithTags("Jobs Lists");
-
-app.MapPost(
-        "/api/jobs-list/jobs/",
-        async (
-            CreateJob request,
-            IJobListService service) =>
-        {
-            (Guid jobListId, string? title, DateTime due) = request;
-
-            await service.AddJob(jobListId, title, due);
-
-            return Results.Ok();
-        })
-    .Accepts<CreateJob>("application/json")
-    .Produces(200)
-    .Produces<ErrorResponse>(400)
-    .Produces<ErrorResponse>(404)
-    .WithTags("Jobs");
-
-app.MapPut(
-        "/api/jobs-list/jobs/complete",
-        async (
-            CompleteJob request,
-            IJobListService service) =>
-        {
-            (Guid jobListId, Guid jobId) = request;
-
-            await service.CompleteJob(jobListId, jobId);
-
-            return Results.Ok();
-        })
-    .Accepts<CompleteJob>("application/json")
-    .Produces(200)
-    .Produces<ErrorResponse>(400)
-    .Produces<ErrorResponse>(404)
-    .WithTags("Jobs");
-
-app.MapGet(
-        "/api/jobs-list/{id}",
-        async (
-            Guid id,
-            string username,
-            [FromServices] IJobTrackerReadService readService) =>
-        {
-            JobsListDto? jobsList = await readService.FindJobsListAsync(id, username);
-
-            return jobsList is null
-                ? Results.NoContent()
-                : Results.Ok(jobsList);
-        })
-    .Produces(200)
-    .Produces(204)
-    .WithTags("Jobs Lists");
-
-app.MapGet(
-        "/api/jobs-list/",
-        async (
-            string username,
-            [FromServices] IJobTrackerReadService readService) =>
-        {
-            IEnumerable<JobsListDto> jobLists = await readService.FindJobsListAsync(username);
-
-            return jobLists.Any()
-                ? Results.Ok(jobLists)
-                : Results.NoContent();
-        })
-    .Produces(200)
-    .Produces(204)
-    .WithTags("Jobs Lists");
-
-app.MapGet(
-        "/api/jobs-list/jobs/",
-        async (
-            Guid jobListId,
-            [FromServices] IJobTrackerReadService readService) =>
-        {
-            IEnumerable<JobDto> jobs = await readService.FindJobsForJobsListAsync(jobListId);
-
-            return jobs.Any()
-                ? Results.Ok(jobs)
-                : Results.NoContent();
-        })
-    .Produces(200)
-    .Produces(204)
-    .WithTags("Jobs");
+app.MapJobEndpoints();
+app.MapJobsListsEndpoints();
 
 app.Run();
