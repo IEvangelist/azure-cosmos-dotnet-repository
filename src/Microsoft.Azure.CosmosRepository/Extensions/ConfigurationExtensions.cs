2f362980c7d91040e1816130af6d9003323e65e8
Billy Mumby <billy.mumby@outlook.com>
2022-03-15 19:52:46 +0000

    Added ConfigurationExtensions

---
+++ src/Microsoft.Azure.CosmosRepository/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+// Copyright (c) IEvangelist. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Azure.CosmosRepository.Options;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Azure.CosmosRepository.Extensions
+{
+    /// <summary>
+    /// Some useful extensions to pull values that will ve populated into <see cref="RepositoryOptions"/> from configuration
+    /// </summary>
+    public static class ConfigurationExtensions
+    {
+        /// <summary>
+        /// Gets the connection string value from configuration.
+        /// </summary>
+        /// <param name="configuration">The <see cref="IConfiguration"/> instance to read teh config from.</param>
+        public static string GetCosmosRepositoryConnectionString(this IConfiguration configuration) =>
+            configuration.GetValue<string>(
+                RepositoryOptions.CosmosConnectionStringConfigKey);
+
+        /// <summary>
+        /// Gets the database ID value from configuration.
+        /// </summary>
+        /// <param name="configuration">The <see cref="IConfiguration"/> instance to read teh config from.</param>
+        public static string GetCosmosRepositoryDatabaseId(this IConfiguration configuration) =>
+            configuration.GetValue<string>(RepositoryOptions.DatabaseIdConfigKey);
+    }
+}
