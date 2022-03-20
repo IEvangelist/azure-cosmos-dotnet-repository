c6ffee57379e2caf3ad9305a827309c8ce20c51d
Billy Mumby <billy.mumby@outlook.com>
2022-03-20 18:53:45 +0000

    Added some basic docs around projections.

---
+++ samples/Microsoft.Azure.CosmosEventSourcing/EventSourcingCustomerAccount/Projections/WelcomeLetterProjection.cs
@@ -0,0 +1,9 @@
+// Copyright (c) IEvangelist. All rights reserved.
+// Licensed under the MIT License.
+
+namespace EventSourcingCustomerAccount.Projections;
+
+public class WelcomeLetterProjection
+{
+    
+}
