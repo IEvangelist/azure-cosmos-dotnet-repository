37b955faeed64f6436dbafe1dec8b7b5c4386ffc
Evan Johnson <17952965+evancjohnson@users.noreply.github.com>
2022-07-23 14:16:14 -0500

    Adding FirstAsync(), FirstOrDefaultAsync(), LastAsync(), and LastOrDefaultAsync() extension methods (#289)

--- src/Microsoft.Azure.CosmosRepository/Extensions/ValueTaskExtensions.cs
+++ src/Microsoft.Azure.CosmosRepository/Extensions/ValueTaskExtensions.cs
@@ -22,5 +22,55 @@ namespace Microsoft.Azure.CosmosRepository.Extensions
             IEnumerable<T> items = await valueTask;
             return items.ToList();
         }
+
+        /// <summary>
+        /// Returns the first element in the sequence or the default.
+        /// </summary>
+        /// <typeparam name="T">The type of entity.</typeparam>
+        /// <param name="valueTask">The value task</param>
+        /// <returns>Returns the first item in the enumerable or the default.</returns>
+        public static async ValueTask<T> FirstOrDefaultAsync<T>(this ValueTask<IEnumerable<T>> valueTask)
+        {
+            IEnumerable<T> items = await valueTask;
+            return items.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the first element in the sequence.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="valueTask">The value task</param>
+        /// <exception cref="System.InvalidOperationException">The source sequence is empty.</exception>
+        /// <returns>Returns the first element in the sequence.</returns>
+        public static async ValueTask<T> FirstAsync<T>(this ValueTask<IEnumerable<T>> valueTask)
+        {
+            IEnumerable<T> items = await valueTask;
+            return items.First();
+        }
+
+        /// <summary>
+        /// Returns the last element in the sequence.
+        /// </summary>
+        /// <typeparam name="T">The entity type.</typeparam>
+        /// <param name="valueTask">The value task</param>
+        /// <returns>Returns the last element in the sequence.</returns>
+        public static async ValueTask<T> LastOrDefaultAsync<T>(this ValueTask<IEnumerable<T>> valueTask)
+        {
+            IEnumerable<T> items = await valueTask;
+            return items.LastOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the last element in the sequence.
+        /// </summary>
+        /// <typeparam name="T">The entity type</typeparam>
+        /// <param name="valueTask">The value task</param>
+        /// <exception cref="System.InvalidOperationException">The source sequence is empty.</exception>
+        /// <returns></returns>
+        public static async ValueTask<T> LastAsync<T>(this ValueTask<IEnumerable<T>> valueTask)
+        {
+            IEnumerable<T> items = await valueTask;
+            return items.Last();
+        }
     }
 }
