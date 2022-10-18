a6a1122bdc60b602b8191a07e67690c804a003fb
Martin Oehlert <mo1987@gmx.net>
2022-10-18 20:50:06 +0200

    Acknowledge serialization options of CosmosClient for PatchOperationBuilder (#302)
    * Acknowledge serialization options of CosmosClient for PatchOperationBuilder
    
    - fixes #270
    
    * Update src/Microsoft.Azure.CosmosRepository/Builders/PatchOperationBuilder.cs
    
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    * Update src/Microsoft.Azure.CosmosRepository/Builders/PatchOperationBuilder.cs
    
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    * Update src/Microsoft.Azure.CosmosRepository/Builders/PatchOperationBuilder.cs
    
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    * Update src/Microsoft.Azure.CosmosRepository/Builders/PatchOperationBuilder.cs
    
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    * fix indexer usage
    
    Co-authored-by: David Pine <david.pine@microsoft.com>

--- tests/Microsoft.Azure.CosmosRepositoryTests/Builders/PatchOperationBuilderTests.cs
+++ tests/Microsoft.Azure.CosmosRepositoryTests/Builders/PatchOperationBuilderTests.cs
@@ -1,12 +1,13 @@
 // Copyright (c) IEvangelist. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.CosmosRepository;
 using Microsoft.Azure.CosmosRepository.Builders;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Xunit;
 
 namespace Microsoft.Azure.CosmosRepositoryTests.Builders
@@ -44,7 +45,7 @@ namespace Microsoft.Azure.CosmosRepositoryTests.Builders
             builder.Replace(x => x.TestProperty, "100");
 
             //Assert
-            PatchOperation operation = builder.PatchOperations.First();
+            PatchOperation operation = builder.PatchOperations[0];
             Assert.Equal(PatchOperationType.Replace, operation.OperationType);
             Assert.Equal("/thisIsTheName", operation.Path);
         }
@@ -59,7 +60,7 @@ namespace Microsoft.Azure.CosmosRepositoryTests.Builders
             builder.Replace(x => x.TestIntProperty, 50);
 
             //Assert
-            PatchOperation operation = builder.PatchOperations.First();
+            PatchOperation operation = builder.PatchOperations[0];
             Assert.Equal(PatchOperationType.Replace, operation.OperationType);
             Assert.Equal("/testIntProperty", operation.Path);
         }
@@ -74,7 +75,7 @@ namespace Microsoft.Azure.CosmosRepositoryTests.Builders
             builder.Replace(x => x.TestProperty, "Test Value");
 
             //Assert
-            PatchOperation operation = builder.PatchOperations.First();
+            PatchOperation operation = builder.PatchOperations[0];
             Assert.Equal(PatchOperationType.Replace, operation.OperationType);
             Assert.Equal("/testProperty", operation.Path);
         }
@@ -89,9 +90,45 @@ namespace Microsoft.Azure.CosmosRepositoryTests.Builders
             builder.Replace(x => x.TestProperty, "Test Value");
 
             //Assert
-            PatchOperation operation = builder.PatchOperations.First();
+            PatchOperation operation = builder.PatchOperations[0];
             Assert.Equal(PatchOperationType.Replace, operation.OperationType);
             Assert.Equal("/testProperty", operation.Path);
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestCases))]
+        public void AcknowledgeRepositorySerializationSettingForRetrievingPatchOperation(CosmosPropertyNamingPolicy? propertyNamingPolicy,
+            string expectedPropertyName)
+        {
+            //Arrange
+            IPatchOperationBuilder<Item1> builder = new PatchOperationBuilder<Item1>(propertyNamingPolicy);
+
+            //Act
+            builder.Replace(x => x.TestIntProperty, 1234);
+
+            //Assert
+            PatchOperation operation = builder.PatchOperations[0];
+            Assert.Equal(PatchOperationType.Replace, operation.OperationType);
+            Assert.Equal($"/{expectedPropertyName}", operation.Path);
+        }
+
+        public static IEnumerable<object?[]> GetTestCases()
+        {
+            yield return new object?[]
+            {
+                CosmosPropertyNamingPolicy.CamelCase,
+                new CamelCaseNamingStrategy().GetPropertyName(nameof(Item1.TestIntProperty), false)
+            };
+            yield return new object?[]
+            {
+                CosmosPropertyNamingPolicy.Default,
+                new DefaultNamingStrategy().GetPropertyName(nameof(Item1.TestIntProperty), false)
+            };
+            yield return new object?[]
+            {
+                null,
+                new CamelCaseNamingStrategy().GetPropertyName(nameof(Item1.TestIntProperty), false)
+            };
         }
     }
 }
