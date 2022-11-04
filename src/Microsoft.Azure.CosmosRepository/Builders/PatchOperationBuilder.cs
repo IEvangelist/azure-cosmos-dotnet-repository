469937cecf10cce0bad00ff1582fd9a4252cdd06
Evan Johnson <ejohnson@envisionedsolution.com>
2022-11-04 02:19:50 -0500

    Starting to expand patch operations

--- src/Microsoft.Azure.CosmosRepository/Builders/PatchOperationBuilder.cs
+++ src/Microsoft.Azure.CosmosRepository/Builders/PatchOperationBuilder.cs
@@ -40,6 +40,69 @@ namespace Microsoft.Azure.CosmosRepository.Builders
             return this;
         }
 
+        public IPatchOperationBuilder<TItem> Set<TValue>(Expression<Func<TItem, TValue>> expression, TValue? value)
+        {
+            PropertyInfo property = expression.GetPropertyInfo();
+            string propertyToReplace = GetPropertyToReplace(property);
+            _rawPatchOperations.Add(new InternalPatchOperation(property, value, PatchOperationType.Set));
+            _patchOperations.Add(PatchOperation.Set($"/{propertyToReplace}", value));
+            return this;
+        }
+
+        public IPatchOperationBuilder<TItem> Add<TValue>(Expression<Func<TItem, TValue>> expression, TValue? value)
+        {
+            PropertyInfo property = expression.GetPropertyInfo();
+            string propertyToReplace = GetPropertyToReplace(property);
+            _rawPatchOperations.Add(new InternalPatchOperation(property, value, PatchOperationType.Replace));
+            _patchOperations.Add(PatchOperation.Add($"/{propertyToReplace}", value));
+            return this;
+        }
+
+        public IPatchOperationBuilder<TItem> Set<TValue>(string path, TValue? value)
+        {
+            _patchOperations.Add(PatchOperation.Set($"/{path}", value));
+            return this;
+        }
+
+        public IPatchOperationBuilder<TItem> Add<TValue>(string path, TValue? value)
+        {
+            _patchOperations.Add(PatchOperation.Add($"/{path}", value));
+            return this;
+        }
+
+        public IPatchOperationBuilder<TItem> Remove<TValue>(Expression<Func<TItem, TValue>> expression)
+        {
+            PropertyInfo property = expression.GetPropertyInfo();
+            string propertyToReplace = GetPropertyToReplace(property);
+            _rawPatchOperations.Add(new InternalPatchOperation(property, null, PatchOperationType.Remove));
+            _patchOperations.Add(PatchOperation.Remove($"/{propertyToReplace}"));
+            return this;
+        }
+
+        public IPatchOperationBuilder<TItem> Remove<TValue>(string path)
+        {
+            _patchOperations.Add(PatchOperation.Remove($"/{path}"));
+            return this;
+        }
+
+        public IPatchOperationBuilder<TItem> Increment(string path, long value)
+        {
+            _patchOperations.Add(PatchOperation.Increment($"/{path}", value));
+            return this;
+        }
+
+        public IPatchOperationBuilder<TItem> Increment(string path, double value)
+        {
+            _patchOperations.Add(PatchOperation.Increment($"/{path}", value));
+            return this;
+        }
+
+        /// <summary>
+        /// Get the property name to replace. This only works for a single level of nesting.
+        /// </summary>
+        /// <remarks>If you're looking for nesting call the respective method with a given path instead.</remarks>
+        /// <param name="propertyInfo">The property to get the path of.</param>
+        /// <returns>Returns the path of the property name.</returns>
         private string GetPropertyToReplace(MemberInfo propertyInfo)
         {
             JsonPropertyAttribute[] attributes =
