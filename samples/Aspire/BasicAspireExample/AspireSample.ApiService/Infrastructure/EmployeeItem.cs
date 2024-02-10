ceb5f3887a81517a13dec0a7dfde3e0bc63ba932
Billy Mumby <billy.mumby@outlook.com>
2024-02-10 17:19:13 +0000

    fix: logical partition query
    Looks as if you have to set the Newtonsoft property name attribute for this to work, feels a bit meh but is the only thing I can get to work atm.
    
    I assume setting some global NewtonsoftJson settings for the library to use camel case may also work.

--- samples/Aspire/BasicAspireExample/AspireSample.ApiService/Infrastructure/EmployeeItem.cs
+++ samples/Aspire/BasicAspireExample/AspireSample.ApiService/Infrastructure/EmployeeItem.cs
@@ -1,7 +1,9 @@
 // Copyright (c) David Pine. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Text.Json.Serialization;
 using Aspire.Microsoft.Azure.CosmosRepository.Items;
+using Newtonsoft.Json;
 
 namespace AspireSample.ApiService.Infrastructure;
 
@@ -27,6 +29,8 @@ public class EmployeeItem : Item
 
     public string LastName { get; set; } = null!;
 
+    [JsonPropertyName("storeNumber")]
+    [JsonProperty("storeNumber")]
     public string StoreNumber { get; set; } = null!;
 
     public DateTimeOffset JoinedAtUtc { get; set; }
