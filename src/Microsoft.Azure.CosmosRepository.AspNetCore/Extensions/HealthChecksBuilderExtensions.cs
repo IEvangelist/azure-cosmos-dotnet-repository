ee6e2217aa517d92a0ee253bdf739489a862734b
Philip Reed <Philip_Reed@next.co.uk>
2024-03-07 11:51:58 +0000

    [feat] Extend HealthChecks to allow AzureCosmosDbHealthCheckOptions to be provided

--- src/Microsoft.Azure.CosmosRepository.AspNetCore/Extensions/HealthChecksBuilderExtensions.cs
+++ src/Microsoft.Azure.CosmosRepository.AspNetCore/Extensions/HealthChecksBuilderExtensions.cs
@@ -55,4 +55,32 @@ public static class HealthChecksBuilderExtensions
 
         return builder;
     }
+
+    /// <summary>
+    /// Add a health check for Azure Cosmos DB by registering <see cref="AzureCosmosDbHealthCheck"/> for given <paramref name="builder"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="IHealthChecksBuilder"/> to add <see cref="HealthCheckRegistration"/> to.</param>
+    /// <param name="healthCheckName">The health check name. Optional. If <c>null</c> the name 'azure_cosmosdb' will be used.</param>
+    /// <param name="failureStatus">
+    /// The <see cref="HealthStatus"/> that should be reported when the health check fails. Optional. If <c>null</c> then
+    /// the default status of <see cref="HealthStatus.Unhealthy"/> will be reported.
+    /// </param>
+    /// <param name="tags">A list of tags that can be used to filter sets of health checks. Optional.</param>
+    /// <param name="timeout">An optional <see cref="TimeSpan"/> representing the timeout of the check.</param>
+    /// <param name="optionsFactory">The `AzureCosmosDbHealthCheckOptions` to use. Optional. If <c>null</c>, the health check just calls CosmosClient.ReadAccountAsync.</param>
+    /// <returns>The specified <paramref name="builder"/>.</returns>
+    public static IHealthChecksBuilder AddCosmosRepository(this IHealthChecksBuilder builder,
+        string? healthCheckName = "azure_cosmosdb",
+        HealthStatus? failureStatus = default,
+        IEnumerable<string>? tags = default,
+        TimeSpan? timeout = default,
+        Func<IServiceProvider, AzureCosmosDbHealthCheckOptions>? optionsFactory = default
+    )
+    {
+        builder.AddAzureCosmosDB(
+            clientFactory: provider => provider.GetRequiredService<ICosmosClientProvider>().CosmosClient,
+            optionsFactory: optionsFactory, healthCheckName, failureStatus, tags, timeout);
+
+        return builder;
+    }
 }
